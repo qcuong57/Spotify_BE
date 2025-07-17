27f1cc17413aaeeb7f4efee8a623d756d5c68fc1
qcuong57 <hoquoccuong945@gmail.com>
2025-07-17 23:45:18 +0700

    Fix: setting.py

--- Spotify_BE/settings.py
+++ Spotify_BE/settings.py
@@ -59,6 +59,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
     "https://spotify-fe.vercel.app",
+    "https://spotify-fe-rho.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 

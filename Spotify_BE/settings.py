dfbd559bc61693abaa240183202a84ca3ffd1353
qcuong57 <hoquoccuong945@gmail.com>
2025-07-17 18:50:44 +0700

    fix login

--- Spotify_BE/settings.py
+++ Spotify_BE/settings.py
@@ -192,3 +192,6 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv("GOOGLE_CLIENT_ID")
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ['email', 'profile']
 SOCIAL_AUTH_REDIRECT_URI = 'http://localhost:5173/auth/callback'
+
+CORS_ALLOW_ALL_ORIGINS = True  # chỉ dùng khi dev
+MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
